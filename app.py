ec665ac2764e225509599090efe0e91917b786b7
Rithb898 <105077299+Rithb898@users.noreply.github.com>
2024-10-14 17:40:46 +0530

    a

--- app.py
+++ app.py
@@ -1,54 +1,63 @@
-from flask import Flask, render_template, request, jsonify, redirect
-import random, string
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
+db = SQLAlchemy(app)
 
-# In-memory database (for demo purposes)
-url_db = {}
+class URL(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(500))
+    short_url = db.Column(db.String(10))
 
-# Function to generate short URL
+with app.app_context():
+    db.create_all()
+
+import random
+import string
+
 def generate_short_url():
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+    letters = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    while True:
+        short_url = ''.join(random.choice(letters) for i in range(6))
+        url = URL.query.filter_by(short_url=short_url).first()
+        if not url:
+            return short_url
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if request.method == 'POST':
+        original_url = request.form['url']
+        url = URL.query.filter_by(original_url=original_url).first()
+        if url:
+            return redirect(url_for('display_short_url', short_url=url.short_url))
+        else:
+            short_url = generate_short_url()
+            new_url = URL(original_url=original_url, short_url=short_url)
+            db.session.add(new_url)
+            db.session.commit()
+            return redirect(url_for('display_short_url', short_url=short_url))
+    return render_template('home.html')
 
-@app.route('/shorten', methods=['POST'])
-def shorten_url():
-    long_url = request.form.get('url')
-    
-    # Validate input
-    if not long_url:
-        return jsonify({'error': 'URL is required'}), 400
-    
-    # Generate short URL
-    short_url = generate_short_url()
-    
-    # Save to "database"
-    url_db[short_url] = long_url
-    
-    return jsonify({'result_url': request.host_url + short_url})
 
 @app.route('/<short_url>')
-def redirect_url(short_url):
-    long_url = url_db.get(short_url)
-    
-    if long_url:
-        return redirect(long_url)
+def display_short_url(short_url):
+    url = URL.query.filter_by(short_url=short_url).first()
+    if url:
+        return render_template('short_url.html', original_url=url.original_url, short_url=url.short_url)
     else:
-        return jsonify({'error': 'Invalid short URL'}), 404
+        return f'Short URL {short_url} not found'
 
-@app.route('/api/v1/shorten', methods=['POST'])
-def api_shorten_url():
-    data = request.get_json()
-    long_url = data.get('url')
-    
-    if not long_url:
-        return jsonify({'error': 'URL is required'}), 400
-    
-    short_url = generate_short_url()
-    url_db[short_url] = long_url
-    
-    return jsonify({'result_url': request.host_url + short_url})
+
+@app.route('/<short_url>/redirect')
+def redirect_to_original_url(short_url):
+    url = URL.query.filter_by(short_url=short_url).first()
+    if url:
+        return redirect(url.original_url)
+    else:
+        return f'Short URL {short_url} not found'
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
